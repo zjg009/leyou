6e56258565eb6d42a99b7b2e2db9afc3229ea802
zjg009 <1364396515@qq.com>
2020-08-25 22:19:44 +0800

    eclipse

--- leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemServiceApplication.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemServiceApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
-
+/**
+ * 
+ * @author Zhoujg
+ *
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.leyou.item.mapper") // mapper接口的包扫描
